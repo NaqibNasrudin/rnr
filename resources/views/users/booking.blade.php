83d9164cd4b8ecd305d6dd2a6db629814a08f419
Naqib Nasrudin <naqibnas1012@gmail.com>
2022-11-24 12:09:46 +0800

    asdasda

--- resources/views/users/booking.blade.php
+++ resources/views/users/booking.blade.php
@@ -7,18 +7,18 @@
         <label for="price">Price: </label>
         <input type="range" min="1" max="50" class="slider">
     </div>
-    <a href="/Book/Booking_form">
-        <div class="card">
-            @foreach ($data as $data)
-                <div class="card_content">
-                    <h3>{{ $data->plate_number }}</h3>
-                    <img src="{{asset("img/{$data->img_name}")}}" alt="">
-                    <hr>
-                    <p>{{$data->brand}} {{$data->model}}</p>
-                    <h4>{{$data->price}}</h4>
-                </div>
-            @endforeach
-        </div>
-    </a>
+    <div class="card">
+        @foreach ($data as $data)
+        <a href="/Book/{{$data->vehicle_id}}/Booking_form">
+            <div class="card_content">
+                <h3>{{ $data->plate_number }}</h3>
+                <img src="{{asset("img/{$data->img_name}")}}" alt="">
+                <hr>
+                <p>{{$data->brand}} {{$data->model}}</p>
+                <h4>{{$data->price}}</h4>
+            </div>
+        </a>
+        @endforeach
+    </div>
 </div>
 @endsection
